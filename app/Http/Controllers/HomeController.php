ad01916a6cf38f5ccb8529b9270f4c5aa3a478db
Smakouz Illea <illeasmakouz@gmail.com>
2024-10-23 13:26:57 +0300

    обновленны основные файлы

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,20 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Home');
+        return Inertia::render('Home',[
+            'title' => 'Home',
+        ]);
+    }
+    public function about()
+    {
+        return Inertia::render('About/About',[
+            'title' => 'About',
+        ]);
+    }
+    public function contacts()
+    {
+        return Inertia::render('Contacts/Contacts',[
+            'title' => 'Contacts',
+        ]);
     }
 }
