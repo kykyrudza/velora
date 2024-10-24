0df09fb8857e957170730fa3fbcbc49c289f3c65
Smakouz Illea <illeasmakouz@gmail.com>
2024-10-24 20:12:04 +0300

    add login controller for backend

--- resources/js/Layouts/Components/Header.vue
+++ resources/js/Layouts/Components/Header.vue
@@ -5,19 +5,19 @@
                 <Link
                     :href="route('home')"
                     class="text-sm font-semibold leading-6 text-gray-900 px-5 py-2"
-                    >
+                >
                     Home
                 </Link>
                 <Link
                     :href="route('about')"
                     class="text-sm font-semibold leading-6 text-gray-900 px-5 py-2"
-                    >
+                >
                     About
                 </Link>
                 <Link
                     :href="route('contacts')"
                     class="text-sm font-semibold leading-6 text-gray-900 px-5 py-2"
-                    >
+                >
                     Contacts
                 </Link>
             </div>
